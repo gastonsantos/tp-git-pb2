192b4e6c264f278b51c5b95543a8fc76256f167e
gus-iturri <gust.i.one@hotmail.com.ar>
2020-04-20 02:10:42 -0300

    Mejora en los metodos de operaciones

--- Calculadora.java
+++ Calculadora.java
@@ -2,15 +2,41 @@ import java.util.Scanner;
 
 public class Ahorcado {
 
-    public static void main(String[] args) {
+
             Integer a;
             Integer b;
             Integer opcion;
             Integer resultado;
+
+private Ahorcado (Integer a,Integer b){
+		this.a = a;
+		this.b = b;		
+	}
+	
+	private Integer suma() {
+		return a + b;
+		
+	}
+	private Integer resta() {
+		return a - b;
+		
+	}
+	private Integer divide() {
+		return a/b;
+		
+	}
+	private Integer multiplica() {
+		return a * b;
+		
+	}
+	
+
             do{
-            System.out.println("Ingrese su operación /n 1 para sumar /n 2 para
+
+            System.out.println("Ingrese su operación /n 1 para        		 sumar /n 2 para
             restar /n 3 para multiplicar /n 4 para dividir");
             opcion = teclado.next();
+
             }while(opcion < 1 && opcion > 4)
 
             System.out.println("Ingrese el primer numero");
@@ -21,17 +47,18 @@ public class Ahorcado {
 
             
     switch (opcion){
+
 		case 4: 
-			resultado = a + b;
+			resultado= suma();
 			break;
 		case 3: 
-			resultado = a - b;
+			resultado = resta();
 			break;
 		case 2: 
-			resultado = a / b;
+			resultado = divide();
 			break;
-		case 1:
-			resultado = a * b ;
+		case 1 :
+			resultado = multiplica();
 			break;
 			
 	           default: 
